a374aa5e6707397fa995505ffe72c2235230c9c1
Florent Gastoud (main) <156147465+gh9869827@users.noreply.github.com>
2025-05-22 22:47:58 -0600

    fix: update uvicorn run command to use full module path

--- fifo_tool_airlock_model_env/server/fastapi_server.py
+++ fifo_tool_airlock_model_env/server/fastapi_server.py
@@ -77,4 +77,7 @@ async def generate(request: InferenceRequestContainerized):
         )
 
 if __name__ == "__main__":
-    uvicorn.run("fastapi_server:app", host="127.0.0.1", port=8000, log_level="info")
+    uvicorn.run("fifo_tool_airlock_model_env.server.fastapi_server:app",
+                host="127.0.0.1",
+                port=8000,
+                log_level="info")
